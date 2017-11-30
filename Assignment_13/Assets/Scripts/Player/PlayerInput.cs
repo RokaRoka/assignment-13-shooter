c43766cd7fb360a15f434447b721257fab11ea31
samscott <samscott497@gmail.com>
2017-11-30 16:00:28 -0500

    Complete Player movement. Added new sprites.

--- Assignment_13/Assets/Scripts/Player/PlayerInput.cs
+++ Assignment_13/Assets/Scripts/Player/PlayerInput.cs
@@ -1,16 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	//Player movement ref
+	private PlayerMovement playerMovement;
+
+	//public delegate void
+	public delegate void PlayerInputEventHandler(object source, EventArgs args);
+
+	//shooting event
+	public event PlayerInputEventHandler PlayerFired;
+
+	//player shoot input delay
+	private float shootInputDelay = 0.1f;
+	private float shootInputCountdown = 0;
+
+	private void Start()
+	{
+		playerMovement = GetComponent<PlayerMovement>();
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update () {
+		InputDelayTick();
+		CheckInput ();
+	}
+
+	private void InputDelayTick()
+	{
+		if (shootInputCountdown > 0)
+			shootInputCountdown -= Time.deltaTime;
+	}
+
+	private void CheckInput()
+	{
+		Vector2 delta = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+			delta.y += 1;
+		if (Input.GetKey(KeyCode.DownArrow))
+			delta.y -= 1;
+		if (Input.GetKey(KeyCode.RightArrow))
+			delta.x += 1;
+		if (Input.GetKey(KeyCode.LeftArrow))
+			delta.x -= 1;
+		if (delta.x != 0 || delta.y != 0)
+			playerMovement.MovePlayer(delta);
+
+		if (Input.GetKeyDown (KeyCode.Space) || shootInputCountdown <= 0) {
+			//player shoot
+		}
+	}
+
+	protected virtual void OnPlayerFired()
+	{
+		if (PlayerFired != null)
+			PlayerFired(this, EventArgs.Empty);
 	}
 }
--- Assignment_13/Assets/Scripts/Player/PlayerMovement.cs
+++ Assignment_13/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	GameObject player;
+	Rigidbody2D rb;
+
+	public float maxVelocity = 15;
+	public float acceleration = 10;
+	private Vector2 currentSpeed = Vector2.zero;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void MovePlayer(Vector2 moveDelta)
+	{
+		currentSpeed = rb.velocity;
+
+		currentSpeed += (acceleration * Time.deltaTime * (rb.drag * 2)) * moveDelta;
+
+		currentSpeed.x = Mathf.Clamp(currentSpeed.x, maxVelocity * -1f, maxVelocity);
+		currentSpeed.y = Mathf.Clamp(currentSpeed.y, maxVelocity * -1f, maxVelocity);
+		//currentSpeed = Vector2.ClampMagnitude(currentSpeed, maxVelocity);
+
+		rb.velocity = currentSpeed;
 	}
 }

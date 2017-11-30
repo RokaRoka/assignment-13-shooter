34764dd1bf67d0fa21a53aa791a2e47fe7679571
samscott <samscott497@gmail.com>
2017-11-30 16:34:40 -0500

    Add lazer prefab.

---
+++ Assignment_13/Assets/Scripts/Projectiles/LazerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerScript : MonoBehaviour {
+
+	private float maxTravelTime = 5f;
+	private float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		if (t > maxTravelTime) {
+			Destroy(gameObject);
+		}
+	}
+}
